49d924110d567f6ac63e0800a7d4049b5b2efbba
DESKTOP-79LIHM0\D6 <bighc0107@naver.com>
2019-05-24 23:31:54 +0900

    Turn Manager

---
+++ Assets/Scripts/TurnMananger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnMananger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public PlayerStats player1 = new PlayerStats(1);
+    public PlayerStats player2 = new PlayerStats(2);
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
