1b165c43fc51e72cd3e85b066acb1df6262107fc
mdechdee <dechdee.m@gmail.com>
2019-05-24 22:47:51 +0900

    Add cameraMovement script

--- Assets/Scripts/TileInfoUIManager.cs
+++ Assets/Scripts/TileInfoUIManager.cs
@@ -8,12 +8,13 @@ public class TileInfoUIManager : MonoBehaviour
     // Start is called before the first frame update
     void onTileSelected(TileClass tile)
     {
-        this.enabled = true;
+        this.GetComponent<CanvasGroup>().alpha = 1f;
         transform.Find("TileInfo_Text").GetComponent<Text>().text = "\t"+tile.tileDescription;
         transform.Find("PollutionMeter/PollutionMeter_Bar").GetComponent<RectTransform>().localScale = new Vector3(tile.polluAmount/tile.maxPolluAmount, 1.0f, 1.0f);
     }
     void onTileUnSelected()
     {
-        this.enabled = false;
+        this.GetComponent<CanvasGroup>().alpha = 0f;
     }
+
 }
---
+++ Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 iniMousePos;
+    private Vector3 iniCamPos;
+    public float camSpeed = 0.05f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(2))
+        {
+            
+            if(iniMousePos == Vector3.zero)
+            {
+                iniMousePos = Input.mousePosition;
+                iniCamPos = this.transform.position;
+                print("ADSAD");
+            }
+            else
+            {
+                print("asd");
+                this.transform.position = (iniCamPos + (iniMousePos - Input.mousePosition)*camSpeed);
+            }
+        }
+        else
+        {
+            print("asd123");
+            iniMousePos = Vector3.zero;
+        }
+    }
+}
