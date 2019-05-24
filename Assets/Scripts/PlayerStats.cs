74235141f6399c85a19a03443cc889b292aaafbd
DESKTOP-79LIHM0\D6 <bighc0107@naver.com>
2019-05-24 20:14:13 +0900

    Player class added with name of PlayerStats

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -2,23 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStats : MonoBehaviour
+public class PlayerStats
 {
-    public int water;
-    public int food;
-    public int metal;
-    public int waste;
-    public int antivaxHP;
-    public int workerHP;
-    // Start is called before the first frame update
-    void Start()
+    public Vector4 resources; //metal, water, food, waste
+    public int antivaxHP_max;
+    public int antivaxHP_present;
+    public int worker_max;
+    public int worker_present;
+    public int support_rate;
+    public int[] unlocked_buildings;
+    public int player_number;
+
+
+    public PlayerStats()
     {
-        
+        player_number = 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    public PlayerStats(int number)
     {
-        
+        player_number = number;
     }
+
+    public void update_resources(Vector4 vec4)
+    {
+        resources = resources + vec4;
+    }
+
+    public void update_antivaxHP(int value)
+    {
+        antivaxHP_present = antivaxHP_present + value;
+        if (antivaxHP_present >= antivaxHP_max)
+        {
+            antivaxHP_present = antivaxHP_max;
+        }
+    }
+
+    public void update_support(int value)
+    {
+        support_rate = support_rate + value;
+    }
+
+    public int get_metal()
+    {
+        return (int)resources[0];
+    }
+    public int get_water()
+    {
+        return (int)resources[1];
+    }
+    public int get_food()
+    {
+        return (int)resources[2];
+    }
+    public int get_waste()
+    {
+        return (int)resources[3];
+    }
+    public int get_worker()
+    {
+        return worker_present;
+    }
+    public int get_antivaxHP()
+    {
+        return antivaxHP_present;
+    }
+    public int get_support()
+    {
+        return support_rate;
+    }
+    
+
 }
