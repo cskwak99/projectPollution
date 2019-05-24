7ec1a7c763b915196da72a9c0dc4a234f6ca9348
DESKTOP-79LIHM0\D6 <bighc0107@naver.com>
2019-05-24 21:30:02 +0900

    Little bit change about name of function in PlayerStats, and add class for building

---
+++ Assets/Scripts/BuildingBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building
+{
+    public int pollution_rate;
+    public Vector4 required_resource;
+    public Vector4 product_resource;
+    public int player_number;
+
+    public void Update_pollution_rate(int value)
+    {
+        pollution_rate = pollution_rate + value;
+        if (pollution_rate > 100)
+        {
+            pollution_rate = 100;
+        }
+    }
+
+    public void Update_produce(Vector4 vec4)
+    {
+        product_resource = product_resource + vec4;
+    }
+
+    
+}
