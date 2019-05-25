5f256231cc66c48f962ef258553dacc8ebc75f32
Kwak <cskwak99@kaist.ac.kr>
2019-05-25 18:10:50 +0900

    add Building Interface

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building
+{
+    public string buildingType; //To specify building type
+    public double efficiency; //To get efficiecny of worker so that evaluate the building work //Maybe change to float
+    public GameObject assignedWorker; //To check whether building can do something
+    public int wasteMk;
+    public int airPoMk;
+
+    public Vec4 getResources(Vec4 remainResources); //For every building, return Vec4 info about resources that player get
+    public int makeAirPo(); //For factory building, make fixed amout of air pollution
+}
