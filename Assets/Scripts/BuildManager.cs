839f6e5ca2fecab733854db54859564e4f760ecb
DESKTOP-79LIHM0\D6 <bighc0107@naver.com>
2019-05-26 20:30:03 +0900

    Need implement of clicking Tile

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class BuildManager : MonoBehaviour
 {
     private GameObject target_tile;
-    private GameObject clone_factory;
+    private GameObject clone_farm;
+    private GameObject clone_waterpump;
+    private GameObject clone_landfill;
+    private GameObject clone_residental;
+    private GameObject clone_mine;
     public GameObject farm;
+    public GameObject waterpump;
+    public GameObject landfill;
+    public GameObject residental;
+    public GameObject mine;
     // Start is called before the first frame update
     void Start()
     {
-        target_tile = GameObject.Find("0400Plain_tile");
+        target_tile = GameObject.Find("0400Plain_tile"); // we should implement it
     }
 
     // Update is called once per frame
@@ -19,10 +27,34 @@ public class BuildManager : MonoBehaviour
         
     }
 
-    public void Init_Factory()
+    public void Init_Farm()
     {
-        clone_factory = Instantiate(farm);
-        clone_factory.transform.parent = target_tile.transform;
-        clone_factory.GetComponent<Building>().setInitial();
+        clone_farm = Instantiate(farm);
+        clone_farm.transform.parent = target_tile.transform;
+        clone_farm.GetComponent<Building>().setInitial();
+    }
+    public void Init_Waterpump()
+    {
+        clone_waterpump = Instantiate(waterpump);
+        clone_waterpump.transform.parent = target_tile.transform;
+        clone_waterpump.GetComponent<Building>().setInitial();
+    }
+    public void Init_Landfill()
+    {
+        clone_landfill = Instantiate(landfill);
+        clone_landfill.transform.parent = target_tile.transform;
+        clone_landfill.GetComponent<Building>().setInitial();
+    }
+    public void Init_Residental()
+    {
+        clone_residental = Instantiate(residental);
+        clone_residental.transform.parent = target_tile.transform;
+        clone_residental.GetComponent<Building>().setInitial();
+    }
+    public void Init_Mine()
+    {
+        clone_mine = Instantiate(mine);
+        clone_mine.transform.parent = target_tile.transform;
+        clone_mine.GetComponent<Building>().setInitial();
     }
 }
