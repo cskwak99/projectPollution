4bdd08e557aed8f78a19f6ed77e259dd45c72ec6
robotjoaa <robotjoaa1@naver.com>
2019-05-26 17:33:17 +0900

    worker script

---
+++ Assets/Scripts/workertestscript/worker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class worker : MonoBehaviour
+{
+    public PlayerStats player;
+    public int hp;
+    public int support;
+    public TileClass location;
+    public GameObject worker_asset;
+    public GameObject worker_obj;
+    public string name;
+    public int turn_left;
+    enum cur_action
+    {
+        idle,
+        moving,
+        constructing,
+        working,
+        aborting
+    }
+    public worker() { }
+    // Start is called before the first frame update
+    public worker(PlayerStats player,string name)
+    {
+        this.player = player;
+        this.name = name;
+        hp = 100;
+        support = 100;
+        location = player.dome;
+        cur_action = idle;
+        turn_left = 0;
+    }
+    void Start()
+    {
+        worker_obj = (GameObject)Instantiate(worker_asset, location.transform.position+new Vector3(0,0.05,0), Quaternion.identity);
+        worker_obj.name = this.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/workertestscript/wtUIManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class wtUIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GUI tileInfo;
+    private GameObject currentOptionList;
+    private wtOptionManager OPM;
+    public bool isMouseOnUI;
+    public void manageUI(TileClass tile)
+    {
+        Destroy(currentOptionList);
+        if (tile != null)
+        {
+            print(tile.gameObject.name);
+            //BroadcastMessage("onTileSelected", tile);
+            //string[] optionList = tile.getOptions();
+            string[] dummyoptionList = { "Worker", "Build", "Building" };
+            currentOptionList = OPM.createOptionPanel("TileOption", gameObject, dummyoptionList, Camera.main.WorldToScreenPoint(tile.transform.position));
+            Transform tmp;
+            GameObject buildOption;
+            string[] buildOptionList = { };
+            GameObject workerOption;
+            string[] workerOptionList = { };
+            GameObject buildingOption;
+            string[] buildingOptionList = { };
+            ////////////////////////BUILD OPTION/////////////////////////////
+            if (tmp = currentOptionList.transform.Find("Build"))
+            {
+                buildOption = tmp.gameObject;
+                buildOption.GetComponent<Button>().onClick.AddListener(/*() => buildingOptionList = tile.getBuildingList()*/() => { this.setActiveOption(buildOption); });
+                //Change from parent option to child options
+                buildOption = OPM.createOptionPanel("BuildOption", buildOption, dummyoptionList, buildOption.transform.position);
+                buildOption.SetActive(false);
+            }
+            ////////////////////////WORKER OPTION///////////////////////////////
+            if (tmp = currentOptionList.transform.Find("Worker"))
+            {
+                workerOption = tmp.gameObject;
+                workerOption.GetComponent<Button>().onClick.AddListener( /*() =>workerOptionList = tile.getBuildingList()*/() => { this.setActiveOption(workerOption); });
+                //Change from parent option to child options
+                workerOption = OPM.createOptionPanel("WorkerOption", workerOption, dummyoptionList, workerOption.transform.position);
+                workerOption.SetActive(false);
+            }
+            ///////////////////////BUILDING OPTION/////////////////////////////////
+            if (tmp = currentOptionList.transform.Find("Building"))
+            {
+                buildingOption = tmp.gameObject;
+                buildingOption.GetComponent<Button>().onClick.AddListener(() => { this.setActiveOption(buildingOption); });
+                //Change from parent option to child options
+                buildingOption = OPM.createOptionPanel("BuildingOption", buildingOption, buildingOptionList, buildingOption.transform.position);
+                buildingOption.SetActive(false);
+            }
+        }
+        else
+        {
+            print("SPACE");
+            //BroadcastMessage("onTileUnSelected");
+        }
+    }
+
+    public void addMouseHoverListener(GameObject obj)
+    {
+        EventTrigger trigger = obj.AddComponent<EventTrigger>();
+        EventTrigger.Entry entry1 = new EventTrigger.Entry();
+        EventTrigger.Entry entry2 = new EventTrigger.Entry();
+        entry1.eventID = EventTriggerType.PointerEnter;
+        entry1.callback.AddListener((data) => { onMouseHoverUI(); });
+        entry2.eventID = EventTriggerType.PointerExit;
+        entry2.callback.AddListener((data) => { onMouseLeaveUI(); });
+        trigger.triggers.Add(entry1);
+        trigger.triggers.Add(entry2);
+    }
+
+    private void setActiveOption(GameObject daOption)
+    {
+        foreach (Transform child in currentOptionList.transform)
+            child.Find(child.name + "Option").gameObject.SetActive(false);
+        daOption.SetActive(true);
+    }
+
+    private void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            addMouseHoverListener(child.gameObject);
+        }
+        OPM = this.GetComponent<wtOptionManager>();
+    }
+
+    public void onMouseHoverUI()
+    {
+        this.isMouseOnUI = true;
+    }
+    public void onMouseLeaveUI()
+    {
+        this.isMouseOnUI = false;
+    }
+}
