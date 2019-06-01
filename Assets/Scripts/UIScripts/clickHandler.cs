b332ab4f42983921a28affabaaf931ec8c8d90bc
mdechdee <dechdee.m@gmail.com>
2019-06-01 18:46:28 +0900

    Show popup when tile need to be selected

--- Assets/Scripts/UIScripts/clickHandler.cs
+++ Assets/Scripts/UIScripts/clickHandler.cs
@@ -15,6 +15,7 @@ public class clickHandler : MonoBehaviour {
     public IEnumerator getDestTile(System.Action<TileClass> passedTile,worker obj)
     {
         this.UIM.isOnDestTileSelection = true;
+        UIM.showPopup("Select a destination tile");
         while (true)
         {
             if (Input.GetMouseButtonDown(0) && !UIM.isMouseOnUI)
